bf60342e4c9d523afa4b6021bef7a528fe49dafa
Jonezia <adamj6636@gmail.com>
2020-03-30 13:56:41 +0800

    sorting buttons

--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import './SortingVisualiser.css';
 import Input from './input/input'
 
+let min = 0
+let max = 0
+let count = 0
+let speed = 0
+
 export default class SortingVisualiser extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
             array: [],
-            count: 50,
-            min: 5,
-            max: 500,
         }
-        this.newArray = this.newArray.bind(this);
     };
-
-    componentWillMount() {
-        this.newArray()
-    }
 
     newArray() {
         const array = [];
-        for (let i = 0; i < this.state.count; i++) {
-            array.push(RandIntFromInterval(this.state.min,this.state.max))
+        for (let i = 0; i < count; i++) {
+            array.push(RandIntFromInterval(min,max))
         };
+        console.log(min)
+        console.log(max)
+        for (let i=0; i<5; i++) {
+        console.log(RandIntFromInterval(min,max))}
+        console.log(array)
         this.setState({array:array});
     }
 
@@ -31,11 +33,11 @@ export default class SortingVisualiser extends React.Component{
             <div className="array-container">
             {this.state.array.map((value,idx) =>
                 <div className="array-bar-container"
-                    key={idx} style={{width:`${100/this.state.count}%`}}>
+                    key={idx} style={{width:`${100/count}%`}}>
                     <div className = "array-bar-container-2">
                         <div className="array-bar"
                         key={idx}
-                        style={{height:`${(1-value/this.state.max)*100}%`}}>
+                        style={{height:`${(1-value/max)*100}%`}}>
                         </div>
                     </div>
                 </div>
@@ -43,18 +45,32 @@ export default class SortingVisualiser extends React.Component{
             </div>
             <div className="bar">
                 <div className="input-column1">
-                    <Input/>
-                    <Input/>
+                    <Input name="min" onChange={(newMin)=>min=Number(newMin)}/>
+                    <Input name="max" onChange={(newMax)=>max=Number(newMax)}/>
                 </div>
                 <div className="input-column2">
-                    <Input/>
-                    <Input/>
+                    <Input name="count" maxlength="3" placeholder="1-300"
+                    onChange={(newCount)=>count=Number(newCount)}/>
+                    <Input name="speed" maxlength="1" placeholder="1-9"
+                    onChange={(newSpeed)=>speed=Number(newSpeed)}/>
                 </div>
-                <button onClick={()=>this.newArray()}>New!</button>
+                <button id="new" onClick={()=>this.newArray()}>New!</button>
+                <div className="button-column1">
+                    <button onClick={()=>this.bubbleSort()}>bubble sort</button>
+                    <button onClick={()=>this.insertionSort()}>insertion sort</button>
+                </div>
+                <div className="button-column2">
+                    <button onClick={()=>this.selectionSort()}>selection sort</button>
+                    <button onClick={()=>this.quickSort()}>quick sort</button>
+                </div>
+                <div className="button-column3">
+                    <button onClick={()=>this.mergeSort()}>merge sort</button>
+                    <button onClick={()=>this.heapSort()}>heap sort</button>
+                </div>
             </div>
         </div>)
     };
 }
 
 let RandIntFromInterval = (min,max) =>
-    Math.floor(Math.random() * (max-min+1) + min)
+    Math.floor(Math.random() * (max-min+1)) + min
--- src/SortingVisualiser/input/input.jsx
+++ src/SortingVisualiser/input/input.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import './input.css';
 
 export default class Input extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        const value = e.target.value;
+        this.props.onChange(value)
+    }
+
     render() {
         return(<div className="input-box">
-                <p className="input-name">Test: </p>
-                <input/>
+                <p className="input-name">{this.props.name}:</p>
+                <input onChange={this.handleChange} maxlength={this.props.maxlength}
+                placeholder={this.props.placeholder}/>
             </div>)
     }
 }
