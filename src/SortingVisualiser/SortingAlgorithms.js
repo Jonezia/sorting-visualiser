ee2c6f926beb66dafe44c98b01493f8b952255eb
Jonezia <adamj6636@gmail.com>
2020-03-31 18:36:16 +0800

    selection sort and red bars for swaps

--- src/SortingVisualiser/SortingAlgorithms.js
+++ src/SortingVisualiser/SortingAlgorithms.js
@@ -1,17 +1,48 @@
 export function bubbleSortAnims(arr) {
     let len = arr.length;
     let animations = []
-    let colors = []
+    let color1 = []
+    let color2 = []
     for (let i = 0; i < len; i++) {
         for (let j = 0; j < len; j++) {
             animations.push(arr.slice());
-            colors.push([j,j+1]);
+            color1.push([j,j+1]);
+            color2.push([])
             if (arr[j] > arr[j + 1]) {
                 [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
                 animations.push(arr.slice());
-                colors.push([j,j+1]);
+                color1.push([])
+                color2.push([j,j+1]);
             }
         }
     }
-    return [animations, colors];
+    return [animations, color1, color2];
 };
+
+export function selectionSortAnims(arr) {
+    let len = arr.length;
+    let animations = [];
+    let color1 = [];
+    let color2 = [];
+    for (let i = 0; i < len; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            animations.push(arr.slice())
+            color1.push([j,min])
+            color2.push([])
+            if (arr[min] > arr[j]) {
+                min = j;
+                animations.push(arr.slice())
+                color1.push([j,min])
+                color2.push([])
+            }
+        }
+        if (min !== i) {
+            [arr[i],arr[min]] = [arr[min],arr[i]]
+            animations.push(arr.slice())
+            color1.push([])
+            color2.push([i,min])
+        }
+    }
+    return [animations, color1, color2];
+}
--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SortingVisualiser.css';
 import Input from './input/input'
-import {bubbleSortAnims} from './SortingAlgorithms.js';
+import {bubbleSortAnims, selectionSortAnims} from './SortingAlgorithms.js';
 
 let min = 0
 let max = 0
@@ -12,13 +12,15 @@ let tid
 let running = false
 
 let color1 = 'orange'
+let color2 = 'red'
 
 export default class SortingVisualiser extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
             array: [],
-            color1: []
+            color1: [],
+            color2: []
         }
     };
 
@@ -29,7 +31,7 @@ export default class SortingVisualiser extends React.Component{
         for (let i = 0; i < count; i++) {
             array.push(RandIntFromInterval(min,max))
         };
-        this.setState({array:array, color1: []});
+        this.setState({array:array, color1: [], color2: []});
     }
 
     bubbleSort() {
@@ -39,16 +41,38 @@ export default class SortingVisualiser extends React.Component{
             if (i == frames[0].length) {
                 running = false
                 clearTimeout(tid);
-                this.setState({color1: []})
+                this.setState({color1: [], color2: []})
             } else {
                 this.setState({array:frames[0][i],
-                    color1: frames[1][i]});
+                    color1: frames[1][i],
+                    color2: frames[2][i]});
                 i += 1
                 tid = setTimeout(bubbleSortRun, delay)
             }
         }}
         running = true
         let tid = setTimeout(bubbleSortRun, delay)
+        let i = 0
+    };
+
+    selectionSort() {
+        let frames = selectionSortAnims(this.state.array)
+        let selectionSortRun = () => {
+            if (running) {
+            if (i == frames[0].length) {
+                running = false
+                clearTimeout(tid);
+                this.setState({color1: [], color2: []})
+            } else {
+                this.setState({array:frames[0][i],
+                    color1: frames[1][i],
+                    color2: frames[2][i]});
+                i += 1
+                tid = setTimeout(selectionSortRun, delay)
+            }
+        }}
+        running = true
+        let tid = setTimeout(selectionSortRun, delay)
         let i = 0
     };
 
@@ -60,7 +84,7 @@ export default class SortingVisualiser extends React.Component{
                     key={idx} style={{width:`${100/count}%`}}>
                     <div className = "array-bar-container-2"
                     style={{backgroundColor: (this.state.color1.includes(idx)) ?
-                    color1 : 'blue'}}>
+                    color1 : (this.state.color2.includes(idx)) ? color2 : 'blue'}}>
                         <div className="array-bar"
                         key={idx}
                         style={{height:`${(1-value/max)*100}%`}}>
